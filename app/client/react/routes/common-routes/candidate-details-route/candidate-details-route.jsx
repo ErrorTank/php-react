cec383891a0ecc984ef16406c6a71b3d6cbb5c7f
TuanAnh <ncq998@gmail.com>
2019-11-05 13:35:01 +0700

    dasdas

--- app/client/react/routes/common-routes/candidate-details-route/candidate-details-route.jsx
+++ app/client/react/routes/common-routes/candidate-details-route/candidate-details-route.jsx
@@ -35,99 +35,134 @@ export class CandidateDetailsRoute extends Component {
                             <div className="route-wrapper">
                                 <div className="container">
                                     <div className="row p-0 m-0">
-                                        <div className="j-header col-12 col-md-10 col-lg-8">
+                                        <div className="j-header col-12 col-md-10 col-lg-9 m-0 p-0">
+                                            <div className="avatar">
+                                                <div className="img-wrapper">
+                                                    <img src={`/assets/image/${candidate.avatar}`}/>
+                                                </div>
+                                            </div>
+                                            <div className={"upper"}>
+                                                <p className="name">{candidate.fullname}</p>
+                                                <p className="label">{candidate.label}</p>
+                                            </div>
+                                            <div className="below">
+                                                <div className="row m-0 p-0">
+                                                    <div className="col-6 m-0 p-0">
+                                                        <i className="far fa-calendar-alt"></i>
+                                                        <span className="value">
+                                                            {moment(new Date(candidate.dob)).format("DD/MM/YYYY")}
+                                                        </span>
+                                                    </div>
+                                                    <div className="col-6 m-0 p-0">
+                                                        {candidate.gender == "1" ? <i className="fas fa-mars"></i> :
+                                                            <i className="fas fa-venus"></i> }
+                                                        <span className="value">
+                                                            {candidate.gender == '1' ? "Nam" : "Nữ"}
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                                <div className="row m-0 p-0" style={{marginTop: "5px !important"}}>
+                                                    <div className="col-6 m-0 p-0">
+                                                        <i className="fas fa-map-marker-alt"></i>
+                                                        <span className="value">
+                                                            {candidate.address}
+                                                        </span>
+                                                    </div>
+                                                    <div className="col-6 m-0 p-0">
+                                                        <i className="fas fa-phone"></i>
+                                                        <span className="value">
+                                                            {candidate.phone}
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                                <div className="row m-0 p-0" style={{marginTop: "5px !important"}}>
+                                                    <div className="col-6 m-0 p-0">
+                                                        <i className="far fa-envelope"></i>
+                                                        <span className="value">
+                                                            {candidate.email}
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="j-body col-12 col-md-10 col-lg-9 m-0">
+                                            <div className="section">
+                                                <div className="fancy-header">
+                                                    <p className="content">Thông tin hồ sơ</p>
+                                                    <p className="underline"></p>
+                                                </div>
+                                                <div className="content">
+                                                    <div className="row p-0 m-0">
+                                                        <div className="item col-6 p-0 m-0">
+                                                            <span className="label">Ngành nghề:</span>
+                                                            <span className="value">
+                                                            {candidate.territories.map((each) => (
+                                                                <Badge
+                                                                    key={each.territoryID}
+                                                                    content={each.label}
+                                                                    className={"blue-badge"}
+                                                                />
+                                                            ))}
+                                                        </span>
+                                                        </div>
+                                                        <div className="item col-6 p-0 m-0">
+                                                            <span className="label">Nơi làm việc:</span>
+                                                            <span className="value">
+                                                            {candidate.places.map((each) => (
+                                                                <Badge
+                                                                    key={each.wpID}
+                                                                    content={each.label}
+                                                                    className={"blue-badge"}
+                                                                />
+                                                            ))}
+                                                        </span>
+                                                        </div>
+                                                        <div className="item col-6 p-0 m-0">
+                                                            <span className="label">Trình độ học vấn:</span>
+                                                            <span className="value">{candidate.selfLevel}</span>
+                                                        </div>
+                                                        <div className="item col-6 p-0 m-0">
+                                                            <span className="label">Loại hình công việc:</span>
+                                                            <span
+                                                                className="value">{candidate.workType == "fulltime" ? "Toàn thời gian" : "Bán thời gian"}</span>
+                                                        </div>
+                                                        <div className="item col-6 p-0 m-0">
+                                                            <span className="label">Cấp bậc mong muốn:</span>
+                                                            <span className="value">{candidate.desiredLevel}</span>
+                                                        </div>
+                                                        <div className="item col-6 p-0 m-0">
+                                                            <span className="label">Mức lương mong muốn:</span>
+                                                            <span className="value text-danger">{(candidate.salaryStart / 1000000).toFixed(0)} - {(candidate.salaryEnd / 1000000).toFixed(0)} Triệu</span>
+                                                        </div>
+                                                        <div className="item col-6 p-0 m-0">
+                                                            <span className="label">Số năm kinh nghiệm:</span>
+                                                            <span className="value text-danger">{candidate.experimentTime} năm</span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="section">
+                                                <div className="fancy-header">
+                                                    <p className="content"> Mục tiêu nghề nghiệp</p>
+                                                    <p className="underline"></p>
+                                                </div>
+                                                <div className="content text-content">
+                                                    {candidate.selfTarget}
+                                                </div>
+
+                                            </div>
+                                            <div className="section">
+                                                <div className="fancy-header">
+                                                    <p className="content">Kỹ năng bản thân</p>
+                                                    <p className="underline"></p>
+                                                </div>
+                                                <div className="content text-content">
+                                                    {candidate.selfSkill}
+                                                </div>
+
+                                            </div>
 
                                         </div>
-                                        {/*<div className="j-body">*/}
-                                        {/*    <div className="section">*/}
-                                        {/*        <div className="fancy-header">*/}
-                                        {/*            <p className="content">Thông tin tuyển dụng nhanh</p>*/}
-                                        {/*            <p className="underline"></p>*/}
-                                        {/*        </div>*/}
-                                        {/*        <div className="content">*/}
-                                        {/*            <div className="row p-0 m-0">*/}
-                                        {/*                <div className="item col-6 p-0 m-0">*/}
-                                        {/*                    <span className="label">Kinh nghiệm:</span>*/}
-                                        {/*                    <span className="value">{job.requiredExperiment}</span>*/}
-                                        {/*                </div>*/}
-                                        {/*                <div className="item col-6 p-0 m-0">*/}
-                                        {/*                    <span className="label">Hình thức làm việc:</span>*/}
-                                        {/*                    <span*/}
-                                        {/*                        className="value">{job.workType == "fulltime" ? "Toàn thời gian" : "Bán thời gian"}</span>*/}
-                                        {/*                </div>*/}
-                                        {/*                <div className="item col-6 p-0 m-0">*/}
-                                        {/*                    <span className="label">Yêu cầu bằng cấp:</span>*/}
-                                        {/*                    <span className="value">{job.requiredLevel}</span>*/}
-                                        {/*                </div>*/}
-                                        {/*                <div className="item col-6 p-0 m-0">*/}
-                                        {/*                    <span className="label">Chức vụ:</span>*/}
-                                        {/*                    <span className="value">{job.desiredLevel}</span>*/}
-                                        {/*                </div>*/}
-                                        {/*                <div className="item col-6 p-0 m-0">*/}
-                                        {/*                    <span className="label">Số lượng cần tuyển:</span>*/}
-                                        {/*                    <span className="value">{job.quantity}</span>*/}
-                                        {/*                </div>*/}
-                                        {/*                <div className="item col-6 p-0 m-0">*/}
-                                        {/*                    <span className="label">Yêu cầu giới tính:</span>*/}
-                                        {/*                    <span*/}
-                                        {/*                        className="value">{job.requiredGender == '1' ? "Nam" : "Nữ"}</span>*/}
-                                        {/*                </div>*/}
-                                        {/*                <div className="item col-12 p-0 m-0">*/}
-                                        {/*                    <span className="label">Ngành nghề:</span>*/}
-                                        {/*                    <span className="value">*/}
-                                        {/*                    {job.territories.map((each) => (*/}
-                                        {/*                        <Badge*/}
-                                        {/*                            key={each.territoryID}*/}
-                                        {/*                            content={each.label}*/}
-                                        {/*                            className={"blue-badge"}*/}
-                                        {/*                        />*/}
-                                        {/*                    ))}*/}
-                                        {/*                </span>*/}
-                                        {/*                </div>*/}
-                                        {/*            </div>*/}
-                                        {/*        </div>*/}
-                                        {/*    </div>*/}
-                                        {/*    <div className="section">*/}
-                                        {/*        <div className="fancy-header">*/}
-                                        {/*            <p className="content">Mô tả công việc</p>*/}
-                                        {/*            <p className="underline"></p>*/}
-                                        {/*        </div>*/}
-                                        {/*        <div className="content text-content">*/}
-                                        {/*            {job.description}*/}
-                                        {/*        </div>*/}
-
-                                        {/*    </div>*/}
-                                        {/*    <div className="section">*/}
-                                        {/*        <div className="fancy-header">*/}
-                                        {/*            <p className="content">Quyền lợi được hưởng</p>*/}
-                                        {/*            <p className="underline"></p>*/}
-                                        {/*        </div>*/}
-                                        {/*        <div className="content text-content">*/}
-                                        {/*            {job.priority}*/}
-                                        {/*        </div>*/}
-
-                                        {/*    </div>*/}
-                                        {/*    <div className="section">*/}
-                                        {/*        <div className="fancy-header">*/}
-                                        {/*            <p className="content">Yêu cầu công việc</p>*/}
-                                        {/*            <p className="underline"></p>*/}
-                                        {/*        </div>*/}
-                                        {/*        <div className="content text-content">*/}
-                                        {/*            {job.jobRequired}*/}
-                                        {/*        </div>*/}
-
-                                        {/*    </div>*/}
-                                        {/*    <div className="section">*/}
-                                        {/*        <div className="fancy-header">*/}
-                                        {/*            <p className="content">Yêu cầu hồ sơ</p>*/}
-                                        {/*            <p className="underline"></p>*/}
-                                        {/*        </div>*/}
-                                        {/*        <div className="content text-content">*/}
-                                        {/*            {job.itemRequired}*/}
-                                        {/*        </div>*/}
-
-                                        {/*    </div>*/}
-                                        {/*</div>*/}
                                         {/*<div className="j-footer">*/}
                                         {/*    <div className="section">*/}
                                         {/*        <div className="fancy-header">*/}
